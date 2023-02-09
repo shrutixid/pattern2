216ed61569b6e9f67c1417f282e45d9f373ed0c1
shrutixid <123883088+shrutixid@users.noreply.github.com>
2023-02-09 20:29:09 +0530

    Add files via upload

---
+++ pattern 2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+ printf("The following code is to print a pattern :-\n\n");
+ for(int i=0;i<=5;i++)
+ {
+  for(int j=0;j<=i;j++)
+  {
+   printf("*");
+  }
+  printf("\n");
+ }
+}
